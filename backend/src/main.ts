23a5d84c620576b0653104393ede1fa020c0914d
Taylor Buckner <taylora.buckner@gmail.com>
2019-09-27 10:57:04 -0500

    Added swagger docs to BE.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as pkg from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Love Pups Backend')
+    .setDescription('Backend for the Love Pups project')
+    .setVersion(pkg.version)
+    .addTag('Love Pups')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
