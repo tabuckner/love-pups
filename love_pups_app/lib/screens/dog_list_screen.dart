fe3ac8167d8e949eba40a5f2e63f25d57b5cc4e4
Taylor Buckner <tbuckner@theappraisallane.com>
2019-11-30 17:17:50 -0600

    Added AppBar to DogCardListScreen.

--- love_pups_app/lib/screens/dog_list_screen.dart
+++ love_pups_app/lib/screens/dog_list_screen.dart
@@ -6,10 +6,24 @@ import 'package:love_pups_app/widgets/dog_card.widget.dart';
 
 class DogListScreen extends StatelessWidget {
   static String routeName = '/dog-list';
+  DogCard activeDogCard;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('Logo'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              print('change card');
+            },
+          )
+        ],
+      ),
       body: Center(
         child: DogCard(dog: DOG_DATA[Random().nextInt(DOG_DATA.length)]),
       ),
